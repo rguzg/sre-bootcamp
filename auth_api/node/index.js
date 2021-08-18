b9a6e02c55f460fe1e0903569e2b87cbf84b0be7
Raúl Guzmán <raulguzmanguerrero@outlook.com>
2021-08-18 18:47:25 -0500

    Load environment variables

--- auth_api/node/index.js
+++ auth_api/node/index.js
@@ -1,3 +1,12 @@
+/* 
+  dotenv/config must be imported first so that all other imported modules 
+  have access to the environment variables
+
+  For more info check out: https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+*/ 
+
+import "dotenv/config";
+
 import Config from 'config';
 import app from './server';
 
