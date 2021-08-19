b18052e5412073a52c76dbfcb7e9fb7dbfbd5869
Raúl <raulguzmanguerrero@outlook.com>
2021-08-19 00:24:49 -0500

    Add db.GetUser tests

--- auth_api/node/test/tests.js
+++ auth_api/node/test/tests.js
@@ -1,6 +1,19 @@
+/* 
+  dotenv/config must be imported first so that all other imported modules 
+  have access to the environment variables
+
+  For more info check out: https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+*/ 
+
+import "dotenv/config";
+
 import chai from 'chai';
+// import * as chaiAsPromised from "chai-as-promised"; 
 import { loginFunction } from '../services/login'
 import { protectFunction } from '../services/protected'
+import * as db from '../services/database';
+
+// chai.use(chaiAsPromised);
 
 const expect = chai.expect;
 
@@ -16,4 +29,26 @@ describe('protectFunction()', function () {
 
     expect("You are under protected data").to.be.equal(protectFunction("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI"));
   });
+});
+
+describe('db.GetUser', () => {
+  it('Requesting an existing user', async () => {
+    let user = await db.GetUser("admin");
+
+    expect("admin").to.be.equal(user.username);
+  });
+
+  it('Requesting a non-existant user', async () => {
+    expect({}).to.eql(await db.GetUser("ThisIsANonExistantUser"));
+  });
+
+  // it('username argument is not a string', async () => {
+  //   (await db.GetUser(5)).should.be.rejected;
+  //   // expect(await db.GetUser({})).to.throw();
+  //   // expect(await db.GetUser(null)).to.throw();
+  // });
+
+  // it('username argument is missing', async () => {
+  //   expect(await db.GetUser()).to.throw();
+  // });
 });
