f073b4a194b1fefde7a8dcccc3cf6e3a83330ed7
Raúl <raulguzmanguerrero@outlook.com>
2021-08-19 19:32:23 -0500

    Add loginFunction

--- auth_api/node/services/login.js
+++ auth_api/node/services/login.js
@@ -1,3 +1,26 @@
-export const loginFunction = (username, password) => {
-  return 'test';
+import { GetUser } from "./database";
+import { ComparePasswords } from "./database";
+import { sign } from "jsonwebtoken";
+
+/**
+ * Returns a JWT if the username and password are correct, otherwise it returns null
+ * @param {String} username 
+ * @param {String} password 
+ * @returns {Promise<String>|Promise<null>}
+ */
+export const loginFunction = async (username, password) => {
+  if(!username || typeof(username) != 'string' || !password || typeof(password) != 'string' ){
+    throw new TypeError("Arguments should be a string");
+  }
+
+  let isPasswordValid = await ComparePasswords(username, password);
+
+  if(isPasswordValid){
+    let {role} = await GetUser(username);
+    let jwt = sign({role}, process.env.SECRET, {noTimestamp: true});
+
+    return jwt
+  }
+  
+  return null;
 }
