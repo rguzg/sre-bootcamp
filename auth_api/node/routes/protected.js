4cb721ae24802cd29285e6632db2959e01211f05
Raúl <raulguzmanguerrero@outlook.com>
2021-08-20 01:51:32 -0500

    Add protected endpoint

--- auth_api/node/routes/protected.js
+++ auth_api/node/routes/protected.js
@@ -1,10 +1,19 @@
 import { protectFunction } from '../services/protected';
 
 export const protect = (req, res, next) => {
-  let authorization = req.headers.authorization;
-  let response = {
-    "data": protectFunction(authorization)
-  };
-  res.send(response);
-  next();
+  if(!req.headers.authorization){
+    return res.status(403).json({status: 403, message: "Forbidden"});
+  }
+
+  let authorization = req.headers.authorization.split('Bearer')[1];
+
+  if(authorization){
+    let message = protectFunction(authorization.trim());
+  
+    if(message){
+      return res.send({"data": message});
+    }
+  }
+
+  return res.status(403).json({status: 403, message: "Forbidden"});
 }
